1f81e7c11f8bcc9032196573574cb516753ff7f0
gilberto <gilberto.vento@gmail.com>
2022-02-15 20:55:51 -0500

    Adding file sign and version to block storage.

--- src/main/java/com/github/jambodb/storage/blocks/FileBlockStorage.java
+++ src/main/java/com/github/jambodb/storage/blocks/FileBlockStorage.java
@@ -3,27 +3,21 @@ package com.github.jambodb.storage.blocks;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SeekableByteChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class FileBlockStorage implements BlockStorage {
-    private static final int HEADER_SIZE = 8;
+    private static final byte[] SIGN = "JamboBlks".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] VERSION = "v1".getBytes(StandardCharsets.UTF_8);
+    private static final int HEADER_SIZE = SIGN.length + VERSION.length + 8;
+
     private final SeekableByteChannel channel;
     private int blockSize;
     private int blockCount;
-
-    public FileBlockStorage(int blockSize, SeekableByteChannel channel) {
-        this.blockSize = blockSize;
-        this.blockCount = 0;
-        this.channel = channel;
-    }
-
-    public FileBlockStorage(SeekableByteChannel channel) throws IOException {
-        this.channel = channel;
-        readHeader();
-    }
 
     public static BlockStorage writeable(int blockSize, Path path) throws IOException {
         OpenOption[] options;
@@ -51,6 +45,17 @@ public class FileBlockStorage implements BlockStorage {
         }
         SeekableByteChannel channel = Files.newByteChannel(path, options);
         return new FileBlockStorage(blockSize, channel);
+    }
+
+    public FileBlockStorage(int blockSize, SeekableByteChannel channel) {
+        this.blockSize = blockSize;
+        this.blockCount = 0;
+        this.channel = channel;
+    }
+
+    public FileBlockStorage(SeekableByteChannel channel) throws IOException {
+        this.channel = channel;
+        readHeader();
     }
 
     @Override
@@ -107,6 +112,8 @@ public class FileBlockStorage implements BlockStorage {
 
     private void writeHeader() throws IOException {
         ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
+        buffer.put(SIGN);
+        buffer.put(VERSION);
         buffer.putInt(blockSize);
         buffer.putInt(blockCount);
         buffer.flip();
@@ -117,10 +124,26 @@ public class FileBlockStorage implements BlockStorage {
     private void readHeader() throws IOException {
         ByteBuffer buffer = ByteBuffer.allocate(HEADER_SIZE);
         channel.position(0);
-        channel.read(buffer);
+        int readed = channel.read(buffer);
+        if(readed != HEADER_SIZE) {
+            throw new IOException("invalid file");
+        }
         buffer.flip();
+        validate(buffer);
         blockSize = buffer.getInt();
         blockCount = buffer.getInt();
+    }
+
+    private void validate(ByteBuffer buffer) throws IOException {
+        byte[] fileSign = new byte[SIGN.length];
+        byte[] fileVersion = new byte[VERSION.length];
+
+        buffer.get(fileSign);
+        buffer.get(fileVersion);
+
+        if(!Arrays.equals(fileSign, SIGN) || !Arrays.equals(fileVersion, VERSION)) {
+            throw new IOException("invalid file");
+        }
     }
 
     @Override
